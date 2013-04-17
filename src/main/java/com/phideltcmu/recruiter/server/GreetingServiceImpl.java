596242f95f157fee203d18b7ec8a807d18ee546f
Mathew Gray <matgray@cmu.edu>
2013-04-17 05:09:19 -0400

    Uses Java Bean to instantiate DAO

--- src/main/java/com/phideltcmu/recruiter/server/GreetingServiceImpl.java
+++ src/main/java/com/phideltcmu/recruiter/server/GreetingServiceImpl.java
@@ -1,48 +1,57 @@
 package com.phideltcmu.recruiter.server;
 
-import com.phideltcmu.recruiter.client.GreetingService;
-import com.phideltcmu.recruiter.shared.FieldVerifier;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.phideltcmu.recruiter.client.GreetingService;
+import com.phideltcmu.recruiter.server.dao.RecruitListDao;
+import com.phideltcmu.recruiter.shared.FieldVerifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * The server side implementation of the RPC service.
  */
 @SuppressWarnings("serial")
 public class GreetingServiceImpl extends RemoteServiceServlet implements
-    GreetingService {
+        GreetingService {
+    private ApplicationContext context = new ClassPathXmlApplicationContext("/amzaws/Spring-Module.xml");
 
-  public String greetServer(String input) throws IllegalArgumentException {
-    // Verify that the input is valid. 
-    if (!FieldVerifier.isValidName(input)) {
-      // If the input is not valid, throw an IllegalArgumentException back to
-      // the client.
-      throw new IllegalArgumentException(
-          "Name must be at least 4 characters long");
+    private RecruitListDao recruitListDao = (RecruitListDao) context.getBean("recruitListDao");
+
+    public String greetServer(String input) throws IllegalArgumentException {
+        // Verify that the input is valid.
+        if (!FieldVerifier.isValidName(input)) {
+            // If the input is not valid, throw an IllegalArgumentException back to
+            // the client.
+            throw new IllegalArgumentException(
+                    "Name must be at least 4 characters long");
+        }
+
+        String serverInfo = getServletContext().getServerInfo();
+        String userAgent = getThreadLocalRequest().getHeader("User-Agent");
+
+        // Escape data from the client to avoid cross-site script vulnerabilities.
+        input = escapeHtml(input);
+        userAgent = escapeHtml(userAgent);
+
+        recruitListDao.create("foo", "bar", "foobar");
+        recruitListDao.selectAll();
+
+        return "Hello, " + input + "!<br><br>I am running " + serverInfo
+                + ".<br><br>It looks like you are using:<br>" + userAgent;
     }
 
-    String serverInfo = getServletContext().getServerInfo();
-    String userAgent = getThreadLocalRequest().getHeader("User-Agent");
-
-    // Escape data from the client to avoid cross-site script vulnerabilities.
-    input = escapeHtml(input);
-    userAgent = escapeHtml(userAgent);
-
-    return "Hello, " + input + "!<br><br>I am running " + serverInfo
-        + ".<br><br>It looks like you are using:<br>" + userAgent;
-  }
-
-  /**
-   * Escape an html string. Escaping data received from the client helps to
-   * prevent cross-site script vulnerabilities.
-   * 
-   * @param html the html string to escape
-   * @return the escaped string
-   */
-  private String escapeHtml(String html) {
-    if (html == null) {
-      return null;
+    /**
+     * Escape an html string. Escaping data received from the client helps to
+     * prevent cross-site script vulnerabilities.
+     *
+     * @param html the html string to escape
+     * @return the escaped string
+     */
+    private String escapeHtml(String html) {
+        if (html == null) {
+            return null;
+        }
+        return html.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(
+                ">", "&gt;");
     }
-    return html.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(
-        ">", "&gt;");
-  }
 }
