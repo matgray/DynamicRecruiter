064e200aa0c7470d8732e9df51d670990e561c6d
Mathew Gray <matgray@cmu.edu>
2013-04-21 22:21:12 -0400

    Initial commit of panel for adding people to list

--- src/main/java/com/phideltcmu/recruiter/client/ui/PersonAddPanel.java
+++ src/main/java/com/phideltcmu/recruiter/client/ui/PersonAddPanel.java
@@ -1,25 +1,57 @@
 package com.phideltcmu.recruiter.client.ui;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.shared.SimpleEventBus;
+import com.google.gwt.user.cellview.client.CellTable;
 import com.google.gwt.user.client.ui.*;
+import com.phideltcmu.recruiter.client.DynamicRecruiter;
+import com.phideltcmu.recruiter.client.event.SearchCompletedEvent;
+import com.phideltcmu.recruiter.client.event.SearchCompletedEventHandler;
+import com.phideltcmu.recruiter.client.handler.SearchDirectoryHandler;
+import com.phideltcmu.recruiter.shared.model.Person;
 
-public class PersonAddPanel extends VerticalPanel {
+public class PersonAddPanel extends VerticalPanel implements SearchCompletedEventHandler {
+    Button searchButton = new Button("Search");
+    TextBox searchField = new TextBox();
+    private SimpleEventBus privateEventBus = new SimpleEventBus();
+    PersonTable table = new PersonTable();
+
     public PersonAddPanel() {
+        privateEventBus.addHandler(SearchCompletedEvent.TYPE, this);
+
         FlexTable layout = new FlexTable();
         layout.setCellSpacing(6);
         FlexTable.FlexCellFormatter cellFormatter = layout.getFlexCellFormatter();
 
-        layout.setHTML(0, 0, "Add a Person");
+        layout.setHTML(0, 0, "Find a person");
         cellFormatter.setColSpan(0, 0, 2);
         cellFormatter.setHorizontalAlignment(0, 0, HasHorizontalAlignment.ALIGN_CENTER);
 
-        layout.setHTML(1, 0, "Name");
-        layout.setWidget(1, 1, new TextBox());
+        layout.setHTML(1, 0, "Search Text");
+        layout.setWidget(1, 1, searchField);
 
-        layout.setHTML(2, 0, "Andrew ID");
-        layout.setWidget(2, 1, new TextBox());
+        layout.setWidget(2, 0, searchButton);
+        cellFormatter.setColSpan(2, 0, 2);
+        cellFormatter.setHorizontalAlignment(2, 0, HasHorizontalAlignment.ALIGN_CENTER);
+
+        searchButton.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent clickEvent) {
+                DynamicRecruiter.RECRUIT_SERVICE.search(searchField.getText(), new SearchDirectoryHandler(privateEventBus));
+            }
+        });
+
+        CellTable<Person> searchResults = new CellTable<Person>();
 
         DecoratorPanel infoPanel = new DecoratorPanel();
         infoPanel.setWidget(layout);
+        this.setWidth("100%");
         this.add(infoPanel);
+    }
+
+    @Override
+    public void onSearchCompleted(SearchCompletedEvent event) {
+        table.setData(event.getPersonList());
     }
 }
