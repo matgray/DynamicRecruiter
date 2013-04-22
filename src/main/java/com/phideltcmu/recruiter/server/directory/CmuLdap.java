14e043030ee6d820b1dd84d7fb5d4971e5a7211e
Mathew Gray <matgray@cmu.edu>
2013-04-22 04:42:36 -0400

    Correctly queries CMU LDAP server for username / andrew id.  Also now fetches class year (freshman, sophomore, etc.)

--- src/main/java/com/phideltcmu/recruiter/server/directory/CmuLdap.java
+++ src/main/java/com/phideltcmu/recruiter/server/directory/CmuLdap.java
@@ -4,14 +4,16 @@ import com.phideltcmu.recruiter.shared.model.Person;
 import com.unboundid.ldap.sdk.*;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CmuLdap {
 
     /**
      * Get the name and department associated with the AndrewID
      */
-    public static String[] fields = {"cn", "cmuDepartment", "cmuAndrewId", "mail"};
+    public static String[] fields = {"cn", "cmuDepartment", "cmuAndrewId", "mail", "cmuStudentClass"};
 
     /**
      * Get attributes associated with the searchText (Currently just the full
@@ -36,21 +38,27 @@ public class CmuLdap {
          * For each field, query the server
          */
         SearchResult searchResults = connection.search("ou=ANDREWPERSON,dc=ANDREW,dc=cmu,dc=edu",
-                SearchScope.SUB, "(cn~=" + searchText + ")", fields[0], fields[1], fields[2], fields[3]);
+                SearchScope.SUB, "(|(cn=*" + searchText + "*)(cmuAndrewId=" + searchText + "))", fields[0], fields[1], fields[2], fields[3], fields[4]);
 
         /**
          * In the case that there are multiple entries for a field,
          * just choose the first one
          */
+
+        Set<String> seenAndrewIDs = new HashSet<String>();
+
         for (int i = 0; i < searchResults.getEntryCount(); i++) {
             SearchResultEntry entry = searchResults.getSearchEntries().get(i);
-            Person p = new Person();
-            String[] nameSplit = entry.getAttributeValue("cn").split(" ");
-            p.setFirstName(nameSplit[0]);
-            p.setLastName(nameSplit[nameSplit.length - 1]);
-            p.setAndrewID(entry.getAttributeValue("cmuAndrewId"));
-            p.setMajor(entry.getAttributeValue("cmuDepartment"));
-            matchList.add(p);
+            if (!seenAndrewIDs.contains(entry.getAttributeValue("cmuAndrewId"))) {
+                Person p = new Person();
+                String[] nameSplit = entry.getAttributeValue("cn").split(" ");
+                p.setFirstName(nameSplit[0]);
+                p.setLastName(nameSplit[nameSplit.length - 1]);
+                p.setAndrewID(entry.getAttributeValue("cmuAndrewId"));
+                p.setMajor(entry.getAttributeValue("cmuDepartment"));
+                p.setClassYear(entry.getAttributeValue("cmuStudentClass"));
+                matchList.add(p);
+            }
         }
         connection.close();
         return matchList;
