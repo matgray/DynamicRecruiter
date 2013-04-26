90c1a22e7f52c112dd767b504b1063acbf2d5fa2
Mathew Gray <matgray@cmu.edu>
2013-04-25 23:47:04 -0400

    updated searching popup

--- src/main/java/com/phideltcmu/recruiter/client/ui/popup/SearchingPopup.java
+++ src/main/java/com/phideltcmu/recruiter/client/ui/popup/SearchingPopup.java
@@ -11,8 +11,8 @@ public class SearchingPopup extends PopupPanel {
     public SearchingPopup() {
         super();
         VerticalPanel vp = new VerticalPanel();
-        vp.add(new Image("images/ajax-loader2.gif"));
         vp.add(new Label("Querying The CMU Directory..."));
+        vp.add(new Image("images/ajax-loader2.gif"));
         vp.setHorizontalAlignment(HorizontalPanel.ALIGN_CENTER);
         vp.setWidth("100%");
         this.add(vp);
