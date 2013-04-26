7eeba0ad511931e4ca68226ee45ac7d5ac6d0a05
Mathew Gray <matgray@cmu.edu>
2013-04-25 23:51:19 -0400

    mail sending now works from client

--- src/main/java/com/phideltcmu/recruiter/client/ui/MailerPanel.java
+++ src/main/java/com/phideltcmu/recruiter/client/ui/MailerPanel.java
@@ -5,34 +5,48 @@
 
 package com.phideltcmu.recruiter.client.ui;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.shared.SimpleEventBus;
 import com.google.gwt.user.client.DOM;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.RichTextArea;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.*;
+import com.phideltcmu.recruiter.client.DynamicRecruiter;
 import com.phideltcmu.recruiter.client.ui.mail.RichTextToolbar;
 import com.phideltcmu.recruiter.client.ui.popup.MailSendPopup;
+import com.phideltcmu.recruiter.shared.model.Category;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MailerPanel extends VerticalPanel {
 
     private Button sendButton = new Button("Send...");
     private SimpleEventBus privateEventBus = new SimpleEventBus();
     private CategoriesPanel categoriesPanel = new CategoriesPanel(privateEventBus, "Send Email to...", false);
-    private MailSendPopup confirmationPopup = new MailSendPopup();
+    private final RichTextArea TEXT_AREA;
+    private final TextBox subject = new TextBox();
+    private List<Category> categories = new ArrayList<Category>();
+    private final MailerPanel t = this;
+    MailSendPopup mailSendPopup = null;
 
     public MailerPanel() {
-        RichTextArea area = new RichTextArea();
-        RichTextToolbar tb = new RichTextToolbar(area);
+        Grid g = new Grid(1, 2);
+        g.setWidget(0, 0, new Label("Subject:"));
+        g.setWidget(0, 1, subject);
+        this.add(g);
+        TEXT_AREA = new RichTextArea();
+        RichTextToolbar tb = new RichTextToolbar(TEXT_AREA);
         sendButton.setWidth("100%");
 
         HorizontalPanel hp = new HorizontalPanel();
         VerticalPanel p = new VerticalPanel();
         p.add(tb);
-        p.add(area);
+        p.add(TEXT_AREA);
 
-        area.setHeight("14em");
-        area.setWidth("100%");
+        TEXT_AREA.setHeight("14em");
+        TEXT_AREA.setWidth("100%");
         tb.setWidth("100%");
         p.setWidth("100%");
         DOM.setStyleAttribute(p.getElement(), "marginRight", "4px");
@@ -43,5 +57,58 @@ public class MailerPanel extends VerticalPanel {
         vp2.add(sendButton);
         hp.add(vp2);
         this.add(hp);
+
+        sendButton.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent clickEvent) {
+                /**
+                 * Check subject
+                 */
+                if (subject.getText().length() == 0) {
+                    if (Window.confirm("Send without subject?")) {
+                        subject.setText("NO SUBJECT");
+                    } else {
+                        return;
+                    }
+                }
+                /**
+                 * Check body
+                 */
+                if (TEXT_AREA.getText().length() == 0) {
+                    Window.alert("I refuse to send an empty message");
+                    return;
+                }
+                /**
+                 * Check categories
+                 */
+                categories.clear();
+                categories = CategoriesPanel.filterCategories(categoriesPanel.getCheckBooleanMap());
+                mailSendPopup = new MailSendPopup(categories, t);
+                mailSendPopup.generateWarning();
+                mailSendPopup.display("Cancel");
+            }
+        });
     }
+
+    public void sendMail() {
+        DynamicRecruiter.RECRUIT_SERVICE.sendMail(subject.getText(),
+                TEXT_AREA.getHTML(),
+                categories,
+                DynamicRecruiter.authUser.getAuthToken(),
+                new AsyncCallback<Void>() {
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        throwable.printStackTrace();
+                        Window.alert("There was a problem sending the emails...");
+                    }
+
+                    @Override
+                    public void onSuccess(Void aVoid) {
+
+                        mailSendPopup.hide();
+                        Window.alert("The emails were sent");
+                    }
+                });
+    }
+
 }
--- src/main/java/com/phideltcmu/recruiter/client/ui/popup/MailSendPopup.java
+++ src/main/java/com/phideltcmu/recruiter/client/ui/popup/MailSendPopup.java
@@ -5,15 +5,74 @@
 
 package com.phideltcmu.recruiter.client.ui.popup;
 
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.PopupPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.*;
+import com.phideltcmu.recruiter.client.DynamicRecruiter;
+import com.phideltcmu.recruiter.client.ui.MailerPanel;
+import com.phideltcmu.recruiter.shared.model.Category;
+import com.phideltcmu.recruiter.shared.model.Person;
 
-public class MailSendPopup extends PopupPanel {
-    public MailSendPopup() {
-        VerticalPanel vp = new VerticalPanel();
-        Label label = new Label("You are about to send this email to:");
+import java.util.List;
 
+public class MailSendPopup extends HidablePopup {
+    private final VerticalPanel vp = new VerticalPanel();
+    private final MailSendPopup t = this;
+    private List<Category> categories;
+    private MailerPanel caller;
+    private Image loading = new Image("images/ajax-loader.gif");
+    private LoadingPopup lp = new LoadingPopup();
 
+    public MailSendPopup(List<Category> categories, final MailerPanel mailerPanel) {
+        this.categories = categories;
+        this.caller = mailerPanel;
+    }
+
+    public void generateWarning() {
+        vp.add(new Label("You are about to send this email to:"));
+        vp.add(new InlineHTML("<hr><br>"));
+        vp.add(loading);
+        DynamicRecruiter.RECRUIT_SERVICE.getRecruitList(categories, new AsyncCallback<List<Person>>() {
+            @Override
+            public void onFailure(Throwable throwable) {
+                Window.alert("There was a problem getting the email list from the server");
+                vp.remove(loading);
+                t.hide();
+            }
+
+            @Override
+            public void onSuccess(List<Person> persons) {
+                if (persons.size() == 0) {
+                    Window.alert("No one matches the categories specified... Aborting");
+                    t.hide();
+                    return;
+                }
+                vp.remove(loading);
+                for (Person p : persons) {
+                    vp.add(new Label(p.getFullName()));
+                }
+                Button concentButton = new Button("Send it");
+                concentButton.setWidth("100%");
+                concentButton.addClickHandler(new ClickHandler() {
+                    @Override
+                    public void onClick(ClickEvent clickEvent) {
+                        caller.sendMail();
+                        lp.show();
+                    }
+                });
+                vp.add(new InlineHTML("<br><hr><br>"));
+                vp.add(concentButton);
+                vp.add(new InlineHTML("<br>"));
+            }
+        });
+        this.bindWidget(vp);
+    }
+
+    @Override
+    public void hide() {
+        lp.hide();
+        super.hide();
     }
 }
