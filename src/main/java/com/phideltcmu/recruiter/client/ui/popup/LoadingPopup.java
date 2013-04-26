e85e0d481850e74bc53127e73e75884b5e7ae5a1
Mathew Gray <matgray@cmu.edu>
2013-04-25 23:53:36 -0400

    Loading popup no longer displays text, only animation

--- src/main/java/com/phideltcmu/recruiter/client/ui/popup/LoadingPopup.java
+++ src/main/java/com/phideltcmu/recruiter/client/ui/popup/LoadingPopup.java
@@ -5,17 +5,21 @@
 
 package com.phideltcmu.recruiter.client.ui.popup;
 
-import com.google.gwt.user.client.ui.*;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class LoadingPopup extends PopupPanel {
     public LoadingPopup() {
         super();
         VerticalPanel vp = new VerticalPanel();
-        vp.add(new Image("images/ajax-loader.gif"));
-        vp.add(new Label("Loading..."));
+        Image i = new Image("images/ajax-loader.gif");
+        vp.add(i);
         vp.setHorizontalAlignment(HorizontalPanel.ALIGN_CENTER);
         vp.setWidth("100%");
         this.add(vp);
         this.setGlassEnabled(true);
+        vp.setStyleName("cent");
     }
 }
