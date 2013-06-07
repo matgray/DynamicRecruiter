87224e810b8cd0b23a860a80082e5b121e9b41da
Mathew Gray <matgray@cmu.edu>
2013-06-07 01:54:19 -0400

    Notes centered

--- src/main/java/com/phideltcmu/recruiter/client/ui/popup/PersonDetails.java
+++ src/main/java/com/phideltcmu/recruiter/client/ui/popup/PersonDetails.java
@@ -49,7 +49,7 @@ public class PersonDetails extends HidablePopup {
                         person.setNotes(result);
                     }
                 });
-                adder.show();
+                adder.center();
             }
         });
 
